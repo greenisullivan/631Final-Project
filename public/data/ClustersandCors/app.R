a8091226ee8e4dca6a20cee9e4d4249d72da1dec
kchalk <kd.chalkley@gmail.com>
2018-06-19 22:46:02 -0700

    finished slides, many improvements to shiny app

--- public/data/ClustersandCors/app.R
+++ public/data/ClustersandCors/app.R
@@ -16,8 +16,9 @@ library(knitr)
 library(shiny)
 library(RColorBrewer)
 
-nfreqs <- read_csv(here('static','data','redditProject','normal_subset_freqs.csv'))
-redfreqs <- read_csv(here('static','data','redditProject','reduced_normal_subset_freqs'))
+nfreqs <- read_csv('../redditProject/normal_subset_freqs.csv')
+redfreqs <- read_csv('../redditProject/reduced_normal_subset_freqs')
+clusters <- read_csv('../redditProject/centers') 
 
 od_num=65
 pc_num=21
@@ -53,7 +54,7 @@ corlong <- corlong %>%
     orig %in% c('assentfreq','nonflfreq','fillerfreq', 'absolutistfreq')
     ~ 'idk'))
 
-clusters <- read_csv(here('static','data','redditProject','centers')) 
+clusters <- read_csv('../redditProject/centers')
 
 
 pcs <- distinct(corlong['red_dim'])
@@ -61,36 +62,44 @@ cs <- distinct(clusters['X1'])
 
 ui <- fluidPage(
   titlePanel("PCA Correlations"),
-  sidebarLayout(
-    sidebarPanel(
+  fluidRow(
+    column(4,
       selectInput(inputId = "pc",
                   label = "Principle Component/New Dimemsion",
                   choices=pcs,
                   selected='d1')
-    ),
-    selectInput(inputId = "clusterN",
-                label = "Cluster",
-                choices = list("Choice 1" = 1, 
-                               "Choice 2" = 2, 
-                               "Choice 3" = 3,
-                               "Choice 4" = 4, 
-                               "Choice 5" = 5, 
-                               "Choice 6" = 6,
-                               "Choice 7" = 7#, 
-                               #"Choice 8" = 8, 
-                               #"Choice 9" = 9,
-                               #"Choice 10" = 10, 
-                               #"Choice 11" = 11
-                ),
-                selected=1)
+    )
   ),
-  # Main panel for displaying outputs ----
-  mainPanel(
-    plotOutput(outputId = "corPlot"),
-    plotOutput(outputId = "clusterPlot")
-  )
-) 
-
+  fluidRow(
+    column(12,
+      plotOutput(outputId = "corPlot")
+    )
+  ),
+  fluidRow(
+    column(4,
+         selectInput(inputId = "clusterN",
+                     label = "Cluster",
+                     choices = list("Emotions, Diet, Drugs" = 1, 
+                                    "Computers, Crypto, jobs" = 2, 
+                                    "Trading, sports" = 3,
+                                    "Rant, XXX" = 4, 
+                                    "Choice 5" = 5, 
+                                    "Choice 6" = 6,
+                                    "Choice 7" = 7#, 
+                                    #"Choice 8" = 8, 
+                                    #"Choice 9" = 9,
+                                    #"Choice 10" = 10, 
+                                    #"Choice 11" = 11
+                      ),
+                     selected=1
+          )
+    ),
+    column(6,
+      textOutput(outputId = "clusterText"),
+      plotOutput(outputId = "clusterPlot")
+    )
+  ) 
+)
 
 server <- function(input, output) {
   output$corPlot <- renderPlot({
@@ -98,31 +107,38 @@ server <- function(input, output) {
       filter(red_dim==input$pc) %>% 
       mutate(poscor=abs(correlation), sign=case_when(correlation>=0~'pos', correlation<0~'neg'))
     
-    ggplot(workingdf, aes(x=fct_reorder(orig,poscor,.desc = TRUE), y=poscor, color=dictcat, fill=sign))+
+    ggplot(workingdf, aes(x=fct_reorder(orig,correlation,.desc = TRUE), y=poscor, color=sign, fill=dictcat))+
       geom_col()+
-      theme(axis.text.x = element_text(angle = 90, hjust = 1))+
-      scale_fill_manual(values=c("gray80","#FFFFFF"))
+      theme_minimal()+
+      theme(axis.text.x = element_text(angle = 60, hjust=1,size = 10))+
+      scale_fill_manual(values=c("#FF5A5F", "#FFB400", "#007A87",  "#8CE071", "#7B0051", "#00D1C1", "#FFAA91", "#B4A76C", "#9CA299", "#565A5C", "#00A04B", "#E54C20"))+
+      scale_color_manual(values=c('black','white'))+
+      labs(x='Dictionaries', y='Corelation Magnitude', color='Correlation Direction',fill="Dictionary Category", title="Correlation between Principal Components and Dictionaries")
+    
     
   })
-  
+  output$clusterText <- renderText({
+    paste("Cluster", input$clusterN, "includes subreddits such as: \t\t\n",clusters[input$clusterN,'X1'])
+    
+  })
   output$clusterPlot <- renderPlot({
     radar_colour=c("#FF5A5F", "#FFB400", "#007A87",  "#8CE071", "#7B0051", "#00D1C1", "#FFAA91", "#B4A76C", "#9CA299", "#565A5C", "#00A04B", "#E54C20")
-    
-    clusters <- read_csv(here('static','data','redditProject','centers')) 
     
     clusters <- clusters %>%  
       mutate_at(vars(starts_with('d')),rescale)
     
     clusterN=as.numeric(input$clusterN)
-    print(clusterN)
     ggradar(clusters[clusterN,], grid.label.size = 5,
             group.point.size = 3,
             axis.label.size = 4,
             #axis.labels = dlabels,
             legend.text.size = 11,
             group.colours = radar_colour[clusterN])+
-      theme(legend.position="bottom") 
+      theme(legend.position='none')+
+      labs(subtitle="Shapes of Cluster Centers in PCA space")
+    
   })
+  
 }  
   
 shinyApp(ui = ui, server = server)
